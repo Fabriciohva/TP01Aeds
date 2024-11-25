a8be4e976ef51bba6a72e87c8fd59c3d4ba31ebc
Fabriciohva <fabriciohvianaa@gmail.com>
2024-11-24 22:15:22 -0300

    atualizei

--- main.c
+++ main.c
@@ -1,20 +1,173 @@
-/*#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <float.h>
 #include "teste.h"
 
 int main() {
-    FILE *arquivo = fopen("entrada.txt", "r");
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo\n");
-        return 1;
+    int tipo;
+    char linha[500];
+    float latitude, longitude, peso;
+    char mineral1[50], mineral2[50], mineral3[50];
+
+    printf("1-Arquivo de teste  2-Terminal\n");
+    scanf("%d", &tipo);
+
+    if (tipo == 1) {
+        TLista_de_Sondas pLista;
+        int Qsondas;
+        FLSondaVazia(&pLista); // cria uma lista de sondas vazia
+
+        printf("Insira o nome do arquivo de teste: ");
+        fflush(stdout);
+
+        char arquivo[50];
+        scanf("%49s", arquivo);
+
+        FILE *arq = fopen(arquivo, "r"); // abre o arquivo no modo de leitura
+        if (arq == NULL) {
+            printf("Erro ao abrir o arquivo.\n");
+            return 1;
+        }
+
+        fscanf(arq, "%d", &Qsondas);
+        printf("%d\n", Qsondas);
+
+        for (int i = 0; i < Qsondas; i++) {
+
+            Sonda_Espacial sonda;
+            fscanf(arq, "%f %f %f %f %f", &sonda.latitude, &sonda.longitude, &sonda.Capacidade, &sonda.Velocidade, &sonda.Combustivel);
+            printf("%f %f %f %f %f", sonda.latitude, sonda.longitude, sonda.Capacidade, sonda.Velocidade, sonda.Combustivel);
+            Inicializa_Sonda(&sonda, i + 1, sonda.latitude, sonda.longitude);
+
+            if (LSondaInsere(&pLista, &sonda) == 1) {
+                printf(" Sonda %d inserida com sucesso!\n", sonda.Identificador);
+            } else {
+                printf("Falha ao inserir a sonda %d.\n", sonda.Identificador);
+            }
+        }
+
+        printf("Sistema inicializado com %d sondas.\n", Qsondas);
+
+        int Qoperacoes;
+        fscanf(arq, "%d", &Qoperacoes);
+
+        for (int i = 0; i < Qoperacoes; i++) {
+            char Operac;
+            fscanf(arq, " %c", &Operac);
+
+            if (Operac == 'R') {
+                fgets(linha, sizeof(linha), arq);
+                linha[strcspn(linha, "\n")] = '\0';
+
+                char *token = strtok(linha, " ");
+                latitude = atof(token);
+                token = strtok(NULL, " ");
+                longitude = atof(token);
+                token = strtok(NULL, " ");
+                peso = atof(token);
+                token = strtok(NULL, " ");
+                strcpy(mineral1, token ? token : "");
+                token = strtok(NULL, " ");
+                strcpy(mineral2, token ? token : "");
+                token = strtok(NULL, " ");
+                strcpy(mineral3, token ? token : "");
+
+                mineral milist;
+                strcpy(milist.categoria, "");
+                milist.peso = peso;
+                milist.latitude = latitude;
+                milist.longitude = longitude;
+
+                Sonda_Espacial *sondamaisprox = EncontrarSondaMaisProxima(&pLista, latitude, longitude);
+                if (sondamaisprox != NULL) {
+                    Move(sondamaisprox, latitude, longitude);
+                    printf("Sonda %d movida para a posicao da rocha em (%f, %f).\n", sondamaisprox->Identificador, latitude, longitude);
+                    classificacao(&milist, mineral1, mineral2, mineral3);
+                    AdicionarRochaNaSondaMaisProxima(&pLista, &milist);
+                }
+            } else if (Operac == 'I') {
+                if (LSondaEhVazia(&pLista)) {
+                    printf("A lista de sondas esta vazia.\n");
+                } else {
+                    for(int i = 1; i <= Qsondas; i++) {
+                        printf("%d\n", i);
+                        OperacaoI(&pLista);
+                    }
+                }
+            } else if (Operac == 'E') {
+                operacaoE(&pLista, Qsondas);
+            } else {
+                printf("Comando desconhecido: %c\n", Operac);
+            }
+        }
+
+        fclose(arq);
+    } else {
+        TLista_de_Sondas pLista;
+        int Qsondas;
+        FLSondaVazia(&pLista); // cria uma lista de sondas vazia
+
+        printf("Digite a quantidade de sondas: ");
+        scanf("%d", &Qsondas);
+
+        for (int i = 0; i < Qsondas; i++) {
+            Sonda_Espacial sonda;
+            printf("Digite os dados da sonda %d (Latitude Longitude Capacidade Velocidade Combustivel): ", i + 1);
+            scanf("%f %f %f %f %f", &sonda.latitude, &sonda.longitude, &sonda.Capacidade, &sonda.Velocidade, &sonda.Combustivel);
+            Inicializa_Sonda(&sonda, i + 1, sonda.latitude, sonda.longitude);
+
+            if (LSondaInsere(&pLista, &sonda)) {
+                printf("Sonda %d inserida com sucesso!\n", sonda.Identificador);
+            } else {
+                printf("Falha ao inserir a sonda %d.\n", sonda.Identificador);
+            }
+        }
+
+        printf("Sistema inicializado com %d sondas.\n", Qsondas);
+
+        printf("Digite a quantidade de operacoes: ");
+        int Qoperacoes;
+        scanf("%d", &Qoperacoes);
+
+        for (int i = 0; i < Qoperacoes; i++) {
+            char Operac;
+            printf("Digite o comando (R/I/E): ");
+            scanf(" %c", &Operac);
+
+            if (Operac == 'R') {
+                printf("Digite os dados da nova rocha (Latitude Longitude Peso Mineral1 Mineral2 Mineral3): ");
+                scanf("%f %f %f %s %s %s", &latitude, &longitude, &peso, mineral1, mineral2, mineral3);
+
+                mineral milist;
+                strcpy(milist.categoria, "");
+                milist.peso = peso;
+                milist.latitude = latitude;
+                milist.longitude = longitude;
+
+                Sonda_Espacial *sondaMaisProxima = EncontrarSondaMaisProxima(&pLista, latitude, longitude);
+                if (sondaMaisProxima != NULL) {
+                    Move(sondaMaisProxima, latitude, longitude);
+                    printf("Sonda %d movida para a posição da rocha em (%f, %f).\n", sondaMaisProxima->Identificador, latitude, longitude);
+                    classificacao(&milist, mineral1, mineral2, mineral3);
+                    // Adicionar rocha na sonda mais próxima, a função precisa ser implementad
+                }
+            }else if (Operac == 'I') {
+                if (LSondaEhVazia(&pLista)) {
+                    printf("A lista de sondas esta vazia.\n");
+                } else {
+                    for(int i = 1; i <= Qsondas; i++) {
+                        printf("%d\n", i);
+                        OperacaoI(&pLista);
+                    }
+                }
+            } else if (Operac == 'E') {
+                operacaoE(&pLista, Qsondas);
+            } else {
+                printf("Comando desconhecido: %c\n", Operac);
+            }
+        }
     }
-    printf("Arquivo aberto com sucesso\n");
-    TLista_de_Sondas lista;
-    inicializaSistema(&lista, arquivo);
-    //LSondaImprime(&lista);
-    fclose(arquivo);
-    printf("\nArquivo fechado com sucesso\n");
     return 0;
-}*/
+}
